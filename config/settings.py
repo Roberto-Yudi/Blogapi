5554880a0a3ed4bb8b57fc563fdc2f2871026974
Roberto <y.roberto@ymail.com>
2021-06-26 09:35:36 -0300

    Created Custom User

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # Local
+    'accounts',
     'posts',
 ]
 
@@ -126,3 +127,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
